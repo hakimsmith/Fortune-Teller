eaccba879d0dcd44e5418714c5413508b8e5860d
Hakim Smith <hakim.smith19@yahoo.com>
2019-05-15 08:09:32 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Fortune_Teller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "What is your first name";
+
+            Console.WriteLine(name);
+
+            name = "What is your last name?";
+
+            string age = "What is you age?";
+
+            Console.WriteLine(age);
+
+
+
+
+
+        }
+    }
+}
